46c658b807326265bde5feca4f281ac9205de1f5
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2023-01-24 05:26:18 +0300

    feat: Style sidebar

--- app/components/nav/sidebar.jsx
+++ app/components/nav/sidebar.jsx
@@ -1,9 +1,12 @@
 import { sidebarNavLinks } from "~/data/nav-links";
+import Logo from "../logo";
 import NavLinks from "./nav-links";
 
 export default function Sidebar() {
   return (
     <nav className="sidebar-nav">
+      <Logo />
+
       <NavLinks navLinks={sidebarNavLinks} />
     </nav>
   );
