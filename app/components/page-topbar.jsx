7d7044605983a2c0fedfa1097d1e72d69c78eff2
Roudan Chirkoh <roudan.chirkoh@gmail.com>
2023-01-24 17:35:17 +0300

    feat: Update top bar title

--- app/components/page-topbar.jsx
+++ app/components/page-topbar.jsx
@@ -2,8 +2,13 @@ import { useLocation } from "@remix-run/react";
 import { pageTitle } from "~/utils/page-title";
 
 export default function PageTopBar() {
+  // TODO: Get page title from nav-links.js instead.
   const { pathname } = useLocation();
   const pageName = pageTitle(pathname);
 
-  return <header className="capitalize page-header">{pageName}</header>;
+  return (
+    <header className="page-header">
+      <span className="page-header-title capitalize font-500">{pageName}</span>
+    </header>
+  );
 }
